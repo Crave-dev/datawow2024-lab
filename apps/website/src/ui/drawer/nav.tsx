9fd5d97595416f299da80f8c341260d29fcf4e4b
Crave <bersorgertv@gmail.com>
2024-09-22 12:37:03 +0700

    style: change svg property name

--- apps/website/src/ui/drawer/nav.tsx
+++ apps/website/src/ui/drawer/nav.tsx
@@ -28,7 +28,7 @@ function NavDrawer(props: Props) {
     <aside className="pt-[32px] pl-[32px]">
       <button className="py-[32px] px-4" onClick={onClose}>
         <svg className="w-[23px] h-[24px]" width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.10645 7.48804L17.1064 7.48804M17.1064 7.48804L11.1064 1.48804M17.1064 7.48804L11.1064 13.488" stroke="#D8E9E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M0.10645 7.48804L17.1064 7.48804M17.1064 7.48804L11.1064 1.48804M17.1064 7.48804L11.1064 13.488" stroke="#D8E9E4" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
       </button>
       <ul>
